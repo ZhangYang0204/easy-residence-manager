7d5a0879dc1151b168065e16b1a9c79b64d14ea0
ZhangYang <3319527246@qq.com>
2022-08-09 20:37:35 +0800

    1

---
+++ src/main/java/pers/zhangyang/easyresidence/yaml/MessageYaml.java
@@ -0,0 +1,14 @@
+package pers.zhangyang.easyresidence.yaml;
+
+import org.jetbrains.annotations.NotNull;
+import pers.zhangyang.easylibrary.base.YamlBase;
+
+public class MessageYaml extends YamlBase {
+
+    public static final MessageYaml INSTANCE = new MessageYaml();
+
+    private MessageYaml() {
+        super("display/" + SettingYaml.INSTANCE.getDisplay() + "/message.yml");
+    }
+
+}
